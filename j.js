4f0afab80c252b73d088137acdb2a80b78c4dbf6
minhazapon <minhajulabedinapon@gmail.com>
2024-10-01 03:07:19 -0700

    added file

---
+++ j.js
@@ -0,0 +1,358 @@
+
+[
+
+    {
+      "image": "https://img.freepik.com/premium-photo/protein-powder-bottle-bucket-ads-concept-art_726330-3746.jpg?w=826",
+      "name": "Whey Protein",
+      "details": "Gym supplements can be broadly categorized based on their intended use and primary effects.",
+      "price": "2000$"
+   
+    },
+    {
+      "image": "https://img.freepik.com/premium-photo/mockup-bottle-whey-protein-protein-powder-with-fresh-berries_994418-11613.jpg?w=740",
+      "name": "Casein Protein",
+      "details": "Gym supplements can be broadly categorized based on their intended use and primary effects.",
+      "price": "1000$"
+   
+    },
+    {
+      "image": "https://img.freepik.com/premium-photo/muscle-recovery-refuel-high-quality-fresh-healthy-juice-picture-photography_1020697-100896.jpg?w=740",
+      "name": "Soy Protein",
+      "details": "Gym supplements can be broadly categorized based on their intended use and primary effects.",
+      "price": "3000$"
+   
+    },
+    {
+      "image": "https://img.freepik.com/premium-photo/man-hands-protein-shake-nutrition-muscle-gain-body-mass-product-supplement-kitchen-table-hand-male-bodybuilder-preparing-pre-workout-drink-exercise-healthy-meal-home_590464-179862.jpg?w=740",
+      "name": "Pea Protein",
+      "details": "Gym supplements can be broadly categorized based on their intended use and primary effects.",
+      "price": "4000$ "
+   
+    },
+    {
+      "image": "https://www.popsci.com/wp-content/uploads/2021/01/11/6NLD5BZKTRFDBJ5YGVVZAPSW4U.jpg?w=719",
+      "name": "Hemp Protein",
+      "details": "Gym supplements can be broadly categorized based on their intended use and primary effects.",
+      "price": "1200$"
+   
+    },
+    {
+      "image": "https://img.freepik.com/premium-photo/vitamin-fruit-supplement-black-empty-bottle-flavor-splash-berries-generative-ai_845977-4344.jpg?w=740",
+      "name": "Collagen Protein",
+      "details": "Gym supplements can be broadly categorized based on their intended use and primary effects.",
+      "price": "900$"
+   
+    },
+    {
+      "image": "https://img.freepik.com/premium-photo/vitamin-fruit-supplement-black-empty-bottle-flavor-splash-berries-generative-ai_845977-4344.jpg?w=740",
+      "name": "Collagen Protein",
+      "details": "Gym supplements can be broadly categorized based on their intended use and primary effects.",
+      "price": "900$"
+   
+    },
+    {
+      "image": "https://img.freepik.com/premium-photo/vitamin-fruit-supplement-black-empty-bottle-flavor-splash-berries-generative-ai_845977-4344.jpg?w=740",
+      "name": "Collagen Protein",
+      "details": "Gym supplements can be broadly categorized based on their intended use and primary effects.",
+      "price": "900$"
+   
+    },
+    {
+      "image": "https://img.freepik.com/premium-photo/vitamin-fruit-supplement-black-empty-bottle-flavor-splash-berries-generative-ai_845977-4344.jpg?w=740",
+      "name": "Collagen Protein",
+      "details": "Gym supplements can be broadly categorized based on their intended use and primary effects.",
+      "price": "900$"
+   
+    },
+    {
+      "image": "https://img.freepik.com/premium-photo/vitamin-fruit-supplement-black-empty-bottle-flavor-splash-berries-generative-ai_845977-4344.jpg?w=740",
+      "name": "Collagen Protein",
+      "details": "Gym supplements can be broadly categorized based on their intended use and primary effects.",
+      "price": "900$"
+   
+    },
+    {
+      "image": "https://img.freepik.com/premium-photo/vitamin-fruit-supplement-black-empty-bottle-flavor-splash-berries-generative-ai_845977-4344.jpg?w=740",
+      "name": "Collagen Protein",
+      "details": "Gym supplements can be broadly categorized based on their intended use and primary effects.",
+      "price": "900$"
+   
+    },
+    {
+      "image": "https://img.freepik.com/premium-photo/vitamin-fruit-supplement-black-empty-bottle-flavor-splash-berries-generative-ai_845977-4344.jpg?w=740",
+      "name": "Collagen Protein",
+      "details": "Gym supplements can be broadly categorized based on their intended use and primary effects.",
+      "price": "900$"
+   
+    },
+    {
+      "image": "https://img.freepik.com/premium-photo/vitamin-fruit-supplement-black-empty-bottle-flavor-splash-berries-generative-ai_845977-4344.jpg?w=740",
+      "name": "Collagen Protein",
+      "details": "Gym supplements can be broadly categorized based on their intended use and primary effects.",
+      "price": "900$"
+   
+    },
+    {
+      "image": "https://img.freepik.com/premium-photo/vitamin-fruit-supplement-black-empty-bottle-flavor-splash-berries-generative-ai_845977-4344.jpg?w=740",
+      "name": "Collagen Protein",
+      "details": "Gym supplements can be broadly categorized based on their intended use and primary effects.",
+      "price": "900$"
+   
+    },
+    {
+      "image": "https://img.freepik.com/premium-photo/vitamin-fruit-supplement-black-empty-bottle-flavor-splash-berries-generative-ai_845977-4344.jpg?w=740",
+      "name": "Collagen Protein",
+      "details": "Gym supplements can be broadly categorized based on their intended use and primary effects.",
+      "price": "900$"
+   
+    },
+    {
+      "image": "https://img.freepik.com/premium-photo/vitamin-fruit-supplement-black-empty-bottle-flavor-splash-berries-generative-ai_845977-4344.jpg?w=740",
+      "name": "Collagen Protein",
+      "details": "Gym supplements can be broadly categorized based on their intended use and primary effects.",
+      "price": "900$"
+   
+    },
+    {
+      "image": "https://img.freepik.com/premium-photo/vitamin-fruit-supplement-black-empty-bottle-flavor-splash-berries-generative-ai_845977-4344.jpg?w=740",
+      "name": "Collagen Protein",
+      "details": "Gym supplements can be broadly categorized based on their intended use and primary effects.",
+      "price": "900$"
+   
+    },
+    {
+      "image": "https://img.freepik.com/premium-photo/vitamin-fruit-supplement-black-empty-bottle-flavor-splash-berries-generative-ai_845977-4344.jpg?w=740",
+      "name": "Collagen Protein",
+      "details": "Gym supplements can be broadly categorized based on their intended use and primary effects.",
+      "price": "900$"
+   
+    },
+    {
+      "image": "https://img.freepik.com/premium-photo/vitamin-fruit-supplement-black-empty-bottle-flavor-splash-berries-generative-ai_845977-4344.jpg?w=740",
+      "name": "Collagen Protein",
+      "details": "Gym supplements can be broadly categorized based on their intended use and primary effects.",
+      "price": "900$"
+   
+    },
+    {
+      "image": "https://img.freepik.com/premium-photo/vitamin-fruit-supplement-black-empty-bottle-flavor-splash-berries-generative-ai_845977-4344.jpg?w=740",
+      "name": "Collagen Protein",
+      "details": "Gym supplements can be broadly categorized based on their intended use and primary effects.",
+      "price": "900$"
+   
+    },
+    {
+      "image": "https://img.freepik.com/premium-photo/vitamin-fruit-supplement-black-empty-bottle-flavor-splash-berries-generative-ai_845977-4344.jpg?w=740",
+      "name": "Collagen Protein",
+      "details": "Gym supplements can be broadly categorized based on their intended use and primary effects.",
+      "price": "900$"
+   
+    },
+    {
+      "image": "https://img.freepik.com/premium-photo/vitamin-fruit-supplement-black-empty-bottle-flavor-splash-berries-generative-ai_845977-4344.jpg?w=740",
+      "name": "Collagen Protein",
+      "details": "Gym supplements can be broadly categorized based on their intended use and primary effects.",
+      "price": "900$"
+   
+    },
+    {
+      "image": "https://img.freepik.com/premium-photo/vitamin-fruit-supplement-black-empty-bottle-flavor-splash-berries-generative-ai_845977-4344.jpg?w=740",
+      "name": "Collagen Protein",
+      "details": "Gym supplements can be broadly categorized based on their intended use and primary effects.",
+      "price": "900$"
+   
+    },
+    {
+      "image": "https://img.freepik.com/premium-photo/vitamin-fruit-supplement-black-empty-bottle-flavor-splash-berries-generative-ai_845977-4344.jpg?w=740",
+      "name": "Collagen Protein",
+      "details": "Gym supplements can be broadly categorized based on their intended use and primary effects.",
+      "price": "900$"
+   
+    },
+    {
+      "image": "https://img.freepik.com/premium-photo/vitamin-fruit-supplement-black-empty-bottle-flavor-splash-berries-generative-ai_845977-4344.jpg?w=740",
+      "name": "Collagen Protein",
+      "details": "Gym supplements can be broadly categorized based on their intended use and primary effects.",
+      "price": "900$"
+   
+    },
+    {
+      "image": "https://img.freepik.com/premium-photo/vitamin-fruit-supplement-black-empty-bottle-flavor-splash-berries-generative-ai_845977-4344.jpg?w=740",
+      "name": "Collagen Protein",
+      "details": "Gym supplements can be broadly categorized based on their intended use and primary effects.",
+      "price": "900$"
+   
+    },
+    {
+      "image": "https://img.freepik.com/premium-photo/vitamin-fruit-supplement-black-empty-bottle-flavor-splash-berries-generative-ai_845977-4344.jpg?w=740",
+      "name": "Collagen Protein",
+      "details": "Gym supplements can be broadly categorized based on their intended use and primary effects.",
+      "price": "900$"
+   
+    },
+    {
+      "image": "https://img.freepik.com/premium-photo/vitamin-fruit-supplement-black-empty-bottle-flavor-splash-berries-generative-ai_845977-4344.jpg?w=740",
+      "name": "Collagen Protein",
+      "details": "Gym supplements can be broadly categorized based on their intended use and primary effects.",
+      "price": "900$"
+   
+    },
+    {
+      "image": "https://img.freepik.com/premium-photo/vitamin-fruit-supplement-black-empty-bottle-flavor-splash-berries-generative-ai_845977-4344.jpg?w=740",
+      "name": "Collagen Protein",
+      "details": "Gym supplements can be broadly categorized based on their intended use and primary effects.",
+      "price": "900$"
+   
+    },
+    {
+      "image": "https://img.freepik.com/premium-photo/vitamin-fruit-supplement-black-empty-bottle-flavor-splash-berries-generative-ai_845977-4344.jpg?w=740",
+      "name": "Collagen Protein",
+      "details": "Gym supplements can be broadly categorized based on their intended use and primary effects.",
+      "price": "900$"
+   
+    },
+    {
+      "image": "https://img.freepik.com/premium-photo/vitamin-fruit-supplement-black-empty-bottle-flavor-splash-berries-generative-ai_845977-4344.jpg?w=740",
+      "name": "Collagen Protein",
+      "details": "Gym supplements can be broadly categorized based on their intended use and primary effects.",
+      "price": "900$"
+   
+    },
+    {
+      "image": "https://img.freepik.com/premium-photo/vitamin-fruit-supplement-black-empty-bottle-flavor-splash-berries-generative-ai_845977-4344.jpg?w=740",
+      "name": "Collagen Protein",
+      "details": "Gym supplements can be broadly categorized based on their intended use and primary effects.",
+      "price": "900$"
+   
+    },
+    {
+      "image": "https://img.freepik.com/premium-photo/vitamin-fruit-supplement-black-empty-bottle-flavor-splash-berries-generative-ai_845977-4344.jpg?w=740",
+      "name": "Collagen Protein",
+      "details": "Gym supplements can be broadly categorized based on their intended use and primary effects.",
+      "price": "900$"
+   
+    },
+    {
+      "image": "https://img.freepik.com/premium-photo/vitamin-fruit-supplement-black-empty-bottle-flavor-splash-berries-generative-ai_845977-4344.jpg?w=740",
+      "name": "Collagen Protein",
+      "details": "Gym supplements can be broadly categorized based on their intended use and primary effects.",
+      "price": "900$"
+   
+    },
+    {
+      "image": "https://img.freepik.com/premium-photo/vitamin-fruit-supplement-black-empty-bottle-flavor-splash-berries-generative-ai_845977-4344.jpg?w=740",
+      "name": "Collagen Protein",
+      "details": "Gym supplements can be broadly categorized based on their intended use and primary effects.",
+      "price": "900$"
+   
+    },
+    {
+      "image": "https://img.freepik.com/premium-photo/vitamin-fruit-supplement-black-empty-bottle-flavor-splash-berries-generative-ai_845977-4344.jpg?w=740",
+      "name": "Collagen Protein",
+      "details": "Gym supplements can be broadly categorized based on their intended use and primary effects.",
+      "price": "900$"
+   
+    },
+    {
+      "image": "https://img.freepik.com/premium-photo/vitamin-fruit-supplement-black-empty-bottle-flavor-splash-berries-generative-ai_845977-4344.jpg?w=740",
+      "name": "Collagen Protein",
+      "details": "Gym supplements can be broadly categorized based on their intended use and primary effects.",
+      "price": "900$"
+   
+    },
+    {
+      "image": "https://img.freepik.com/premium-photo/vitamin-fruit-supplement-black-empty-bottle-flavor-splash-berries-generative-ai_845977-4344.jpg?w=740",
+      "name": "Collagen Protein",
+      "details": "Gym supplements can be broadly categorized based on their intended use and primary effects.",
+      "price": "900$"
+   
+    },
+    {
+      "image": "https://img.freepik.com/premium-photo/vitamin-fruit-supplement-black-empty-bottle-flavor-splash-berries-generative-ai_845977-4344.jpg?w=740",
+      "name": "Collagen Protein",
+      "details": "Gym supplements can be broadly categorized based on their intended use and primary effects.",
+      "price": "900$"
+   
+    },
+    {
+      "image": "https://img.freepik.com/premium-photo/vitamin-fruit-supplement-black-empty-bottle-flavor-splash-berries-generative-ai_845977-4344.jpg?w=740",
+      "name": "Collagen Protein",
+      "details": "Gym supplements can be broadly categorized based on their intended use and primary effects.",
+      "price": "900$"
+   
+    },
+    {
+      "image": "https://img.freepik.com/premium-photo/vitamin-fruit-supplement-black-empty-bottle-flavor-splash-berries-generative-ai_845977-4344.jpg?w=740",
+      "name": "Collagen Protein",
+      "details": "Gym supplements can be broadly categorized based on their intended use and primary effects.",
+      "price": "900$"
+   
+    },
+    {
+      "image": "https://img.freepik.com/premium-photo/vitamin-fruit-supplement-black-empty-bottle-flavor-splash-berries-generative-ai_845977-4344.jpg?w=740",
+      "name": "Collagen Protein",
+      "details": "Gym supplements can be broadly categorized based on their intended use and primary effects.",
+      "price": "900$"
+   
+    },
+    {
+      "image": "https://img.freepik.com/premium-photo/vitamin-fruit-supplement-black-empty-bottle-flavor-splash-berries-generative-ai_845977-4344.jpg?w=740",
+      "name": "Collagen Protein",
+      "details": "Gym supplements can be broadly categorized based on their intended use and primary effects.",
+      "price": "900$"
+   
+    },
+    {
+      "image": "https://img.freepik.com/premium-photo/vitamin-fruit-supplement-black-empty-bottle-flavor-splash-berries-generative-ai_845977-4344.jpg?w=740",
+      "name": "Collagen Protein",
+      "details": "Gym supplements can be broadly categorized based on their intended use and primary effects.",
+      "price": "900$"
+   
+    },
+    {
+      "image": "https://img.freepik.com/premium-photo/vitamin-fruit-supplement-black-empty-bottle-flavor-splash-berries-generative-ai_845977-4344.jpg?w=740",
+      "name": "Collagen Protein",
+      "details": "Gym supplements can be broadly categorized based on their intended use and primary effects.",
+      "price": "900$"
+   
+    },
+    {
+      "image": "https://img.freepik.com/premium-photo/vitamin-fruit-supplement-black-empty-bottle-flavor-splash-berries-generative-ai_845977-4344.jpg?w=740",
+      "name": "Collagen Protein",
+      "details": "Gym supplements can be broadly categorized based on their intended use and primary effects.",
+      "price": "900$"
+   
+    },
+    {
+      "image": "https://img.freepik.com/premium-photo/vitamin-fruit-supplement-black-empty-bottle-flavor-splash-berries-generative-ai_845977-4344.jpg?w=740",
+      "name": "Collagen Protein",
+      "details": "Gym supplements can be broadly categorized based on their intended use and primary effects.",
+      "price": "900$"
+   
+    },
+    {
+      "image": "https://img.freepik.com/premium-photo/vitamin-fruit-supplement-black-empty-bottle-flavor-splash-berries-generative-ai_845977-4344.jpg?w=740",
+      "name": "Collagen Protein",
+      "details": "Gym supplements can be broadly categorized based on their intended use and primary effects.",
+      "price": "900$"
+   
+    },
+    {
+      "image": "https://img.freepik.com/premium-photo/vitamin-fruit-supplement-black-empty-bottle-flavor-splash-berries-generative-ai_845977-4344.jpg?w=740",
+      "name": "Collagen Protein",
+      "details": "Gym supplements can be broadly categorized based on their intended use and primary effects.",
+      "price": "900$"
+   
+    },
+    {
+      "image": "https://img.freepik.com/premium-photo/vitamin-fruit-supplement-black-empty-bottle-flavor-splash-berries-generative-ai_845977-4344.jpg?w=740",
+      "name": "Collagen Protein",
+      "details": "Gym supplements can be broadly categorized based on their intended use and primary effects.",
+      "price": "900$"
+   
+    },
+   
+   
+   
+   
+]
